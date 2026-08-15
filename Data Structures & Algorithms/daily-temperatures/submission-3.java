038e1c70ab497b9504748ea3c1241eff1a325c5e
Oliver Ord <44840410+OliverOrd@users.noreply.github.com>
2026-08-15 11:06:12 +0100

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        Deque<Integer> stack = new ArrayDeque<>();
+        int[] res = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            int temp = temperatures[i];
+            while(!stack.isEmpty() && 
+            temperatures[stack.peekLast()] < temp) {
+                int prevIndex = stack.pollLast();
+                res[prevIndex] = i - prevIndex;
+            }
+            stack.addLast(i);
+        }
+
+        return res;
+    }
+}
