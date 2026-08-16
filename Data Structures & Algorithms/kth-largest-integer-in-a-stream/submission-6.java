6a4a9a2b73012bdfcd49f9c75c66639ebf8459ba
Oliver Ord <44840410+OliverOrd@users.noreply.github.com>
2026-08-16 19:12:28 +0100

    Add: kth-largest-integer-in-a-stream - submission-6

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-6.java
@@ -0,0 +1,25 @@
+class KthLargest {
+
+    final PriorityQueue<Integer> minHeap = new PriorityQueue<>((a,b) -> Integer.compare(a,b));
+
+    final int capacity;
+
+    public KthLargest(int k, int[] nums) {
+        this.capacity = k;
+        for (int num : nums) {
+            addElement(num);
+        }
+    }
+    
+    public int add(int num) {
+        addElement(num);
+        return minHeap.peek();
+    }
+
+    private void addElement(int num) {
+        minHeap.add(num);
+        if (minHeap.size() > capacity) {
+            minHeap.poll();
+        }
+    }
+}
